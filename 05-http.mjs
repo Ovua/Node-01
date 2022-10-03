2faae692c8a1016928a2bea4e2add11abc76d01a
Ivan Puddighinu <97966255+Ovua@users.noreply.github.com>
2022-10-03 17:08:30 +0200

    Update 05-http.mjs

--- 05-http.mjs
+++ 05-http.mjs
@@ -3,10 +3,9 @@ const http = require('http')
 const server = http.createServer((req, res) => {
 
     res.statusCode == 200 
-        ? (res.setHeader("Content-Type", "text/html"),
-            req.url == '/' && res.end('Welcome'), 
-                 req.url == '/about' && res.end('This is my profile')) 
-        : emit ('error', new Error("Error"), console.log(res.statusCode)) 
+    res.setHeader("Content-Type", "text/html"),
+    req.url == '/' && res.end('Welcome'), 
+    req.url == '/about' && res.end('This is my profile')) 
     }
 )
 
