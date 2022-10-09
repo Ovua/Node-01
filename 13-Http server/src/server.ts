760fe1935087231d75000cf5f16f8a1cfb080418
Ivan Puddighinu <97966255+Ovua@users.noreply.github.com>
2022-10-09 10:01:47 +0200

    Update server.ts

--- 13-Http server/src/server.ts
+++ 13-Http server/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const port = 3000
+const PORT = process.env.PORT||3001
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
